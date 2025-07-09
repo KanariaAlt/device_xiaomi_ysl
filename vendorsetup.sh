6756c719e38f7d9df35117450abbef95bde46839
0xKANARIA <enashinonome264@gmail.com>
2025-07-09 23:36:01 +0000

    ysl: vendorsetup: import partial common tree
    * kernel and kernel module will be added after adaptation is complete :v
    
    Signed-off-by: 0xKANARIA <enashinonome264@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -7,9 +7,30 @@ else
     echo "device/xiaomi/mititanium-common already exists, skipping clone"
 fi
 
+# Clone device camera tree if not exists
+if [ ! -d "device/xiaomi/ysl/camera" ]; then
+    git clone https://github.com/KanariaAlt/device_xiaomi-msm8953_camera -b main device/xiaomi/ysl/camera
+else
+    echo "device/xiaomi/ysl/camera already exists, skipping clone"
+fi
+
 # Clone proprietary if not exists
 if [ ! -d "vendor/xiaomi/ysl" ]; then
     git clone --depth=1 https://github.com/KanariaAlt/proprietary_vendor_xiaomi_ysl -b 13 vendor/xiaomi/ysl
 else
     echo "vendor/xiaomi/ysl already exists, skipping clone"
+fi
+
+# Clone commmon proprietary if not exists
+if [ ! -d "vendor/xiaomi/mititanium-common" ]; then
+    git clone --depth=1 https://github.com/KanariaAlt/proprietary_vendor_xiaomi_mititanium-common -b 13 vendor/xiaomi/mititanium-common
+else
+    echo "vendor/xiaomi/mititanium-common already exists, skipping clone"
+fi
+
+# Clone commmon proprietary if not exists
+if [ ! -d "hardware/titanium_64" ]; then
+    git clone --depth=1 https://github.com/KanariaAlt/proprietary_vendor_xiaomi_mititanium-common -b 13 hardware/titanium_64
+else
+    echo "hardware/titanium_64 already exists, skipping clone"
 fi
