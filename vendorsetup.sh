f9cfbe241c3e01287f37372ee642407bdb2bb9af
0xKANARIA <enashinonome264@gmail.com>
2025-07-11 06:39:40 +0000

    ysl: vendorsetup: update all source to lineage-21
    Signed-off-by: 0xKANARIA <enashinonome264@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,7 +2,7 @@ git clone --depth=1 https://gitlab.com/nekoprjkt/aosp-clang -b 17 prebuilts/clan
 
 # Clone common tree if not exists
 if [ ! -d "device/xiaomi/mititanium-common" ]; then
-    git clone https://github.com/KanariaAlt/device_xiaomi_mititanium-common -b 13 device/xiaomi/mititanium-common
+    git clone https://github.com/KanariaAlt/device_xiaomi_mititanium-common -b 14 device/xiaomi/mititanium-common
 else
     echo "device/xiaomi/mititanium-common already exists, skipping clone"
 fi
@@ -16,14 +16,14 @@ fi
 
 # Clone proprietary if not exists
 if [ ! -d "vendor/xiaomi/ysl" ]; then
-    git clone --depth=1 https://github.com/KanariaAlt/proprietary_vendor_xiaomi_ysl -b 13 vendor/xiaomi/ysl
+    git clone --depth=1 https://github.com/KanariaAlt/proprietary_vendor_xiaomi_ysl -b 14 vendor/xiaomi/ysl
 else
     echo "vendor/xiaomi/ysl already exists, skipping clone"
 fi
 
 # Clone commmon proprietary if not exists
 if [ ! -d "vendor/xiaomi/mititanium-common" ]; then
-    git clone --depth=1 https://github.com/KanariaAlt/proprietary_vendor_xiaomi_mititanium-common -b 13 vendor/xiaomi/mititanium-common
+    git clone --depth=1 https://github.com/KanariaAlt/proprietary_vendor_xiaomi_mititanium-common -b 14 vendor/xiaomi/mititanium-common
 else
     echo "vendor/xiaomi/mititanium-common already exists, skipping clone"
 fi
