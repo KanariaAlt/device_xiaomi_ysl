ce2883fa4f6dff6c712c335e0e4d8e3f32f1dc1a
0xKANARIA <enashinonome264@gmail.com>
2025-07-11 06:29:28 +0000

    ysl: Build missing libraries for 14 QPR3
    Signed-off-by: 0xKANARIA <enashinonome264@gmail.com>

--- device.mk
+++ device.mk
@@ -40,6 +40,9 @@ PRODUCT_PACKAGES += \
     android.hardware.ir@1.0-impl \
     android.hardware.ir@1.0-service
 
+PRODUCT_PACKAGES += \
+    liblzma.vendor
+
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service.xiaomi_msm8953-32
